3a24a3f27d4b9a8cc85591ba3311f6d748e34a2e
FareeKhan <mfaridgul0101@gmail.com>
2025-04-03 09:45:33 +0400

    iosFile

--- ios/AlkwaityAlawalApp/AppDelegate.swift
+++ ios/AlkwaityAlawalApp/AppDelegate.swift
@@ -2,18 +2,34 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import FirebaseCore
+import React
 
 @main
 class AppDelegate: RCTAppDelegate {
+  
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "AlkwaityAlawalApp"
     self.dependencyProvider = RCTAppDependencyProvider()
+    FirebaseApp.configure()
 
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
     self.initialProps = [:]
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  // For handling universal links
+  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    // This method is called when the app is opened via Universal Link
+    return RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
+  }
+
+  // For handling URL schemes and Deeplinks (i.e. for openURL method)
+  override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    // This method is called when the app is opened via a custom URL scheme or Deeplink
+    return RCTLinkingManager.application(application, open: url, options: options)
   }
 
   override func sourceURL(for bridge: RCTBridge) -> URL? {
@@ -22,9 +38,9 @@ class AppDelegate: RCTAppDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
 }
